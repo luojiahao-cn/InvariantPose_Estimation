d11ba604efc6cc9a6f776cc420c4796d7b5c648d
WayneWu0526 <WayneWu0526>
2025-12-03 20:13:15 +0800

    20251203新增计算场和梯度函数文件

--- code/Functions/estimate_pose_ours.m
+++ code/Functions/estimate_pose_ours.m
@@ -82,17 +82,6 @@ R_est = R_est.R; % 返回最终R
 end
 
 %% ----------------------------Functions-------------------------------  %%
-%% Calc magntic field and gradient tensor
-function [b_p, A_p] = calcFieldAndGradient(p, m_pos, m_hat, m_norm)
-b_p = zeros(3, 1);
-A_p = zeros(3, 3);
-for i = 1:size(m_pos,2)
-    r = p - m_pos(:, i);
-    [B, gradB] = dipole_b_and_gradb(r, m_hat(:, i), m_norm(i));
-    b_p = b_p + B;
-    A_p = A_p + gradB;
-end
-end
 %% Estimate p
 function res = obj_fun22(p, m_pos, m_hat, m_norm, num_sensors, b_bar, Q_bar, X)
 [b_p, A_p] = calcFieldAndGradient(p, m_pos, m_hat, m_norm);
---
+++ code/utils/calcFieldAndGradient.m
@@ -0,0 +1,11 @@
+%% Calc magntic field and gradient tensor
+function [b_p, A_p] = calcFieldAndGradient(p, m_pos, m_hat, m_norm)
+    b_p = zeros(3, 1);
+    A_p = zeros(3, 3);
+    for i = 1:size(m_pos,2)
+        r = p - m_pos(:, i);
+        [B, gradB] = dipole_b_and_gradb(r, m_hat(:, i), m_norm(i));
+        b_p = b_p + B;
+        A_p = A_p + gradB;
+    end
+    end
