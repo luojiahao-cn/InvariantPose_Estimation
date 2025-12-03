7f50ee36f873a2e32b9a514ef35d10163bfa5dd6
WayneWu0526 <WayneWu0526>
2025-12-03 20:02:11 +0800

    分离estimateR，删掉与添加color相关的内容

---
+++ code/Functions/estimateR_iter.m
@@ -0,0 +1,35 @@
+%% Estimate R
+function R_struct = estimateR_iter(b_bar, B_bar, A_p, X_opt, R_init, mu, beta)
+    %% Iterative method:
+    LA = min(eig(A_p)) - 1e-3;
+    LX = min(eig(X_opt)) - 1e-3;
+    Abar = A_p - LA * eye(3);
+    Xbar = X_opt - LX * eye(3);
+    M = @(R) 2 * Abar * R * Xbar + mu * B_bar * b_bar';
+    Mbar = @(R) M(R) + beta * R;
+    % f = @(R) trace(R' * A_p * R * X_opt + mu * R' * B_bar * b_bar' + beta * R);
+    % fbar = @(R) trace(R' * At * R * Xt + mu * R' * B_bar * b_bar' + beta * R);
+    % skw = @(R) 0.5 * (R - R');
+    % g = @(R) 2 * skw(R' * (2 * A_p * R * X_opt + mu * B_bar * b_bar' + beta * eye(3)));
+    % gbar = @(R) 2 * skw(R' * (2 * At * R * Xt + mu * B_bar * b_bar' + beta * eye(3)));
+    %% Initial condition
+    delta = 1e6; k = 0; kmax = 1000;
+    R = R_init;
+    R_iter_history = {};
+    delta_history = [];
+    while k < kmax && delta > 1e-5
+        [U, ~, V] = svd(Mbar(R));
+        R_opt = U*diag([1,1,det(U*V')])*V';
+        delta = norm(R_opt - R, 'fro');
+        R_iter_history{end+1} = R_opt;
+        delta_history(end+1) = delta;
+        R = R_opt;
+        k = k + 1;
+    end
+    if k == kmax
+        warning('迭代未收敛，可能需要调整参数');
+    end
+    R_struct.R = R;
+    R_struct.R_iter_history = R_iter_history;
+    R_struct.delta_history = delta_history;
+    end
--- code/Functions/estimate_pose_ours.m
+++ code/Functions/estimate_pose_ours.m
@@ -63,7 +63,7 @@ p_est = lsqnonlin(fun22, p_init, lb_p, ub_p, options);
 [b_p, A_p] = calcFieldAndGradient(p_est, m_pos, m_hat, m_norm);
 B_matrix = b_p * ones(1, num_sensors);
 B_bar = B_matrix * Q_bar;
-[R_init_est1, R_init_est2] = estiamteR(b_bar, B_bar, A_p, X_opt);
+[R_init_est1, R_init_est2] = estimateR(b_bar, B_bar, A_p, X_opt);
 
 L = norm(A_p)*norm(X_opt)/(norm(B_bar)*norm(b_bar));
 % L = 1000;
@@ -100,82 +100,6 @@ term1 = norm(b_p * ones(1, num_sensors) * Q_bar, 'fro') - norm(b_bar, 'fro');
 term2 = trace(A_p*A_p) - trace(X*X);
 term3 = det(A_p) - det(X);
 res = [term1; term2; term3];
-end
-%% Estimate R
-function R_struct = estimateR_iter(b_bar, B_bar, A_p, X_opt, R_init, mu, beta)
-%% Iterative method:
-LA = min(eig(A_p)) - 1e-3;
-LX = min(eig(X_opt)) - 1e-3;
-Abar = A_p - LA * eye(3);
-Xbar = X_opt - LX * eye(3);
-M = @(R) 2 * Abar * R * Xbar + mu * B_bar * b_bar';
-Mbar = @(R) M(R) + beta * R;
-% f = @(R) trace(R' * A_p * R * X_opt + mu * R' * B_bar * b_bar' + beta * R);
-% fbar = @(R) trace(R' * At * R * Xt + mu * R' * B_bar * b_bar' + beta * R);
-% skw = @(R) 0.5 * (R - R');
-% g = @(R) 2 * skw(R' * (2 * A_p * R * X_opt + mu * B_bar * b_bar' + beta * eye(3)));
-% gbar = @(R) 2 * skw(R' * (2 * At * R * Xt + mu * B_bar * b_bar' + beta * eye(3)));
-%% Initial condition
-delta = 1e6; k = 0; kmax = 1000;
-R = R_init;
-R_iter_history = {};
-delta_history = [];
-while k < kmax && delta > 1e-5
-    [U, ~, V] = svd(Mbar(R));
-    R_opt = U*diag([1,1,det(U*V')])*V';
-    delta = norm(R_opt - R, 'fro');
-    R_iter_history{end+1} = R_opt;
-    delta_history(end+1) = delta;
-    R = R_opt;
-    k = k + 1;
-end
-if k == kmax
-    warning('迭代未收敛，可能需要调整参数');
-end
-R_struct.R = R;
-R_struct.R_iter_history = R_iter_history;
-R_struct.delta_history = delta_history;
-end
-
-function [R_opt1, R_opt2] = estiamteR(b_bar, B_bar, A_p, X)
-% estimate R using R^T A R = X
-[PA, LA] = eig(A_p);
-[PX, LX] = eig(X);
-[lam, IndA] = sort(diag(LA), 'ascend');
-PA = PA(:,IndA);
-[sig, IndX] = sort(diag(LX), 'ascend');
-PX = PX(:,IndX);
-
-% ---- 规范为 SO(3) 的特征向量基 ----
-if det(PA) < 0, PA(:,1) = -PA(:,1); end
-if det(PX) < 0, PX(:,1) = -PX(:,1); end
-
-% ---- 枚举 6 个置换，做最佳匹配 ----
-perms3 = perms(1:3);
-bestCost = inf; bestP = eye(3);
-for k = 1:size(perms3,1)
-    p = perms3(k,:);
-    P = eye(3); P = P(:, p);                 % 置换矩阵
-    cost = sum((lam(p) - sig).^2);           % 等价于最大化 sum(sig .* lam(p))
-    if cost < bestCost
-        bestCost = cost; bestP = P;
-    end
-end
-
-% ---- 保证 Y ∈ SO(3) ----
-Y = bestP;
-if det(Y) < 0
-    Y = Y * diag([1 1 -1]);                  % 用符号阵把行列式调成 +1
-end
-
-% ---- 回代得到最优解 R ----
-R_opt1 = PA * Y * PX';
-% R_opt1 = PA * diag(sign(diag(PX'*PA)))*PX';
-
-% estimate R using SVD
-M = B_bar * b_bar';
-[U, ~, V] = svd(M);
-R_opt2 = V*diag([1,1,det(V*U')])*U';
 end
 
 function J = numJacobian(fun, x)
